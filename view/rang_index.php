b1e5cf11fa6beb9d9be03fa9da703c7e43f97160
sokoliva <sokoliva.math@pmf.hr>
2022-07-11 16:12:49 +0200

    rang lista

---
+++ view/rang_index.php
@@ -0,0 +1,14 @@
+<?php require_once __SITE_PATH . '/view/_header.php'; ?>
+<?php require_once __SITE_PATH . '/view/view_util.php'; ?>
+
+<div class="contentcontainer">
+    <div class="card">
+        <?php
+        print_rang($neto, $imena);
+
+        ?>
+    </div>
+</div>
+
+
+<?php require_once __SITE_PATH . '/view/_footer.php'; ?>
